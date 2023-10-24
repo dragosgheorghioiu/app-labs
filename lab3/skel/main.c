3e897b4d509dd8dc2a9ee5e89ba2276ddee22548
dragosgheorghioiu <dragosgheorghioiu9@gmail.com>
2023-10-24 09:22:00 +0300

    lab 3

--- lab3/skel/main.c
+++ lab3/skel/main.c
@@ -25,17 +25,78 @@ int main() {
     // de paralelizat toata bucata de mai jos
 
     // fiti atenti la partea de citire din fisier
-    fscanf(file, "%d", &size);
-    numbers = malloc(size * sizeof(int));
 
-    for (int i = 0; i < size; i++) {
-        fscanf(file, "%d", &numbers[i]);
-    }
-    fclose(file);
+    #pragma omp parallel
+    {
 
-    // de paralelizat in 3 moduri - atomic, critical si reduction, de masurat timpii de executie
-    for (i = 0; i < size; i++) {
-        sum1 += numbers[i];
+        #pragma omp master
+        {
+            fscanf(file, "%d", &size);
+            numbers = malloc(size * sizeof(int));
+
+            for (int i = 0; i < size; i++) {
+                fscanf(file, "%d", &numbers[i]);
+            }
+            fclose(file);
+            t1 = omp_get_wtime();
+        }
+
+        #pragma omp barrier
+
+
+        
+
+        #pragma omp sections private(i)
+        {
+            #pragma omp section
+            {
+                #pragma omp parallel for reduction(+:sum1)
+                for (i = 0; i < size; i++) {
+                    sum1 += numbers[i];
+                }
+                time1 = omp_get_wtime() - t1;
+            }
+    
+            #pragma omp section
+            {
+                for (i = 0; i < size; i++) {
+                    #pragma omp atomic
+                    sum2 += numbers[i];
+                }
+                time2 = omp_get_wtime() - t1;
+            }
+    
+            #pragma omp section
+            {
+                for (i = 0; i < size; i++) {
+                    #pragma omp critical
+                    sum3 += numbers[i];
+                }
+                time3 = omp_get_wtime() - t1;
+            }           
+        }
+
+        #pragma omp sections
+        {
+            #pragma omp section
+            {
+                write_file("output1.txt", "reduction", time1, sum1);
+            }
+    
+            #pragma omp section
+            {
+                write_file("output2.txt", "atomic", time2, sum2);
+            }
+    
+            #pragma omp section
+            {
+                write_file("output3.txt", "critical", time3, sum3);
+            }
+        }
+        
+        
+
+        // de paralelizat in 3 moduri - atomic, critical si reduction, de masurat timpii de executie
     }
 
     // TODO: de scris timpii de executie in 3 fisiere folosind sections (fiecare scriere intr-un section)
